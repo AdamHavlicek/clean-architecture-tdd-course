c520f60bf30746d6bca0d83428cf2a0f2c245752
Adam Havlíček <havlicek.adam23@gmail.com>
2022-12-27 13:39:10 +0100

    fix: widget overflow

--- lib/features/number_trivia/presentation/widgets/trivia_display.dart
+++ lib/features/number_trivia/presentation/widgets/trivia_display.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 
 import '../../domain/entities/number_trivia.dart';
@@ -6,7 +5,10 @@ import '../../domain/entities/number_trivia.dart';
 class TriviaDisplay extends StatelessWidget {
   final NumberTrivia trivia;
 
-  const TriviaDisplay({Key? key, required this.trivia}) : super(key: key);
+  const TriviaDisplay({
+    Key? key,
+    required this.trivia,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +16,23 @@ class TriviaDisplay extends StatelessWidget {
       height: MediaQuery.of(context).size.height / 3,
       child: Expanded(
         child: Center(
-          child: Column(
-            children: [
-              Text(
-                trivia.number.toString(),
-                style: const TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  trivia.number.toString(),
+                  style: const TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SingleChildScrollView(
-                child: Text(
+                Text(
                   trivia.text,
                   style: const TextStyle(fontSize: 25),
                   textAlign: TextAlign.center,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
